640a8bfa4cae29856db5a156a8b212fc844482c4
bdiniscia <bdiniscia@gmail.com>
2020-04-17 16:45:16 -0400

    Styles and final functions of the game

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Image, } from 'react-native';
 import {MaterialCommunityIcons as Icon } from 'react-native-vector-icons';
 import styles from './style.js';
 
@@ -21,6 +21,7 @@ export default class App extends Component {
     this.newGame();
   }
 
+  // Function to restart the game
   newGame = () => {
     this.setState({
       stateOfTheGame: [
@@ -32,8 +33,66 @@ export default class App extends Component {
     })
   }
 
+  checkForDraw = () => {
+    const numOfTitles = 3
+    let arr = this.state.stateOfTheGame
 
+    for (let i=0; i < numOfTitles ; i++) {
+      if (arr[0][i] === 0) {
+        return 0;
+      } else if (arr[1][i] === 0) {
+        return 0;
+      } else if (arr[2][i] === 0) {
+        return 0;
+      }
+    }
+    return 1;
+
+  }
+
+  // Checking if there is a winner
+  checkWinner = () => {
+    const numOfTitles = 3
+    let arr = this.state.stateOfTheGame
+    let sum
+
+    // For rows
+    for (let i = 0; i < numOfTitles; i++) {
+      sum = arr[i][0] + arr[i][1] + arr[i][2]
+      if (sum === 3 ) { return 1 }
+      else if (sum === -3 ) { return -1 }
+    }
+
+    // For columns
+    for (let i = 0; i < numOfTitles; i++) {
+      sum = arr[0][i] + arr[1][i] + arr[2][i]
+      if (sum === 3 ) { return 1 }
+      else if (sum === -3 ) { return -1 }
+    }
+
+    // For Diagonal 1
+    sum = arr[0][0] + arr[1][1] + arr[2][2]
+    if (sum === 3 ) { return 1 }
+    else if (sum === -3 ) { return -1 }
+
+    // For Diagonal 2
+    sum = arr[2][0] + arr[1][1] + arr[0][2]
+    if (sum === 3 ) { return 1 }
+    else if (sum === -3 ) { return -1 }
+
+    // No winners yet
+    return 0;
+  }
+
+
+  // Function for the press of a title
   pressTitle = (row, col) => {
+    // Don't let a marked title change
+    let valueOfTheCell = this.state.stateOfTheGame[row][col]
+    if (valueOfTheCell !== 0) {
+      return
+    }
+
     let currentPlayer = this.state.currentPlayer;
     let arr = this.state.stateOfTheGame.slice();
 
@@ -49,10 +108,25 @@ export default class App extends Component {
       currentPlayer: nextPlayer
     })
 
+    let winner = this.checkWinner()
+    if (winner === 1) {
+      Alert.alert('¡Ganó el Jugador 1!')
+      this.newGame()
+      return
+    } else if (winner === -1) {
+      Alert.alert('¡Ganó el Jugador 2!')
+      this.newGame()
+      return
+    }
 
-
+    let draw = this.checkForDraw()
+    if (draw === 1) {
+      Alert.alert('¡Hubo un empate!')
+      this.newGame()
+    }
   }
 
+  // Fuction to make the icon appear
   renderIcon = (row, col) => {
     let value = this.state.stateOfTheGame[row][col]
     switch(value) {
@@ -62,45 +136,74 @@ export default class App extends Component {
     }
   }
 
+  // To show the current player on screen
+  renderPlayer = () => {
+    let currentPlayer = this.state.currentPlayer
+    switch(currentPlayer) {
+      case 1: return <View style={styles.playerView} ><Text style={styles.player}> Jugador 1 </Text><Icon name='cupcake' style={styles.iconPlayerCupcake}/></View>;
+      case -1: return <View style={styles.playerViewCookie} ><Text style={styles.playerCookie}> Jugador 2 </Text><Icon name='cookie'style={styles.iconPlayerCookie}/></View>;
+    }
+  }
+
   render () {
     return (
       <View style={styles.container}>
-        <View >
-          <TouchableOpacity onPress={() => this.pressTitle(0 ,0)} style={[styles.title, {borderTopWidth: 0, borderLeftWidth:0}] }>
-            {this.renderIcon(0 ,0)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.pressTitle(1 ,0)} style={[styles.title, {borderLeftWidth:0}]}>
-            {this.renderIcon(1 ,0)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.pressTitle(2 ,0)} style={[styles.title, {borderBottomWidth: 0, borderLeftWidth:0}]}>
-            {this.renderIcon(2 ,0)}
-          </TouchableOpacity>
-        </View>
-        
-        <View >
-          <TouchableOpacity onPress={() => this.pressTitle(0 ,1)} style={[styles.title, {borderTopWidth: 0} ]}>
-            {this.renderIcon(0 ,1)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.pressTitle(1 ,1)} style={styles.title}>
-            {this.renderIcon(1 ,1)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.pressTitle(2 ,1)} style={[styles.title, {borderBottomWidth: 0} ]}>
-            {this.renderIcon(2 ,1)}
-          </TouchableOpacity>
+        {/* The Logo */}
+        <Image source={require('./img/logo.png')} style={styles.logo}/>
+
+        {/* The Player ID */}
+        <View style={styles.containerPlayer}>
+          <Text style={styles.gamerID}>Turno de: </Text>
+          {this.renderPlayer()}
         </View>
 
+        {/* THE GRID */}
+        <View style={styles.containerGrid} >
+          <View style={styles.containerRow} >
+            <TouchableOpacity onPress={() => this.pressTitle(0 ,0)} style={[styles.title, {borderTopWidth: 0, borderLeftWidth:0}] }>
+              {this.renderIcon(0 ,0)}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.pressTitle(0 ,1)} style={[styles.title, {borderTopWidth: 0}]}>
+              {this.renderIcon(0 ,1)}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.pressTitle(0 ,2)} style={[styles.title, {borderTopWidth: 0, borderRightWidth:0}]}>
+              {this.renderIcon(0 ,2)}
+            </TouchableOpacity>
+          </View>
+          
+          <View style={styles.containerRow}>
+            <TouchableOpacity onPress={() => this.pressTitle(1 ,0)} style={[styles.title, {borderLeftWidth:0} ]}>
+              {this.renderIcon(1 ,0)}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.pressTitle(1 ,1)} style={styles.title}>
+              {this.renderIcon(1 ,1)}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.pressTitle(1 ,2)} style={[styles.title, {borderRightWidth:0} ]}>
+              {this.renderIcon(1 ,2)}
+            </TouchableOpacity>
+          </View>
 
-        <View >
-          <TouchableOpacity onPress={() => this.pressTitle(0 ,2)} style={[styles.title, {borderTopWidth: 0, borderRightWidth: 0} ]}>
-            {this.renderIcon(0 ,2)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.pressTitle(1 ,2)} style={[styles.title, {borderRightWidth: 0} ]}>
-            {this.renderIcon(1 ,2)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.pressTitle(2 ,2)} style={[styles.title, {borderBottomWidth: 0, borderRightWidth: 0} ]}>
-            {this.renderIcon(2 ,2)}
-          </TouchableOpacity>
+
+          <View style={styles.containerRow}>
+            <TouchableOpacity onPress={() => this.pressTitle(2, 0)} style={[styles.title, {borderBottomWidth: 0, borderLeftWidth: 0} ]}>
+              {this.renderIcon(2, 0)}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.pressTitle(2 ,1)} style={[styles.title, {borderBottomWidth: 0} ]}>
+              {this.renderIcon(2 ,1)}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.pressTitle(2 ,2)} style={[styles.title, {borderBottomWidth: 0, borderRightWidth: 0} ]}>
+              {this.renderIcon(2 ,2)}
+            </TouchableOpacity>
+          </View>
         </View>
+         
+        {/* Button of New Game  */}
+        <View style={{paddingTop: 40}}/>
+        <TouchableOpacity onPress = {() => this.newGame()}>
+          <View style = {styles.buttonNewGame}>
+              <Text style = {{color: '#131515'}}>Juego nuevo</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
--- style.js
+++ style.js
@@ -2,30 +2,110 @@ import {StyleSheet} from 'react-native'
 
 export default StyleSheet.create({
     container: {
+        backgroundColor: '#4ecdc4',
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly'
+    },
+
+    logo: {
+        flex: 1,
+        aspectRatio: 1.8, 
+        resizeMode: 'contain'
+    },
+
+    containerGrid: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+
+    containerRow: {
+        display: 'flex',
         flexDirection: 'row',
-        backgroundColor: '#4ecdc4',
-      },
+    },
 
-      title: {
-          borderWidth: 5, 
-          width: 100, 
-          height: 100,
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderColor: '#fff',
-      },
+    title: {
+        borderWidth: 5, 
+        width: 100, 
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: '#fff',
+    },
 
-      titleCupcake: {
-          color: '#FF1F6C',
-          fontSize: 60,
-      },
-
-      titleCookie: {
-        color: '#FDFF3C',
+    titleCupcake: {
+        color: '#FF1F6C',
         fontSize: 60,
-      }
+    },
+
+    titleCookie: {
+    color: '#FDFF3C',
+    fontSize: 60,
+    },
+
+    buttonNewGame: {
+        backgroundColor: '#FDFF3C', 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 40,
+    },
+
+    containerPlayer: {
+        display: 'flex',
+        flexDirection: 'row',
+        paddingBottom: 40,
+        alignItems: 'center',
+    },
+    gamerID: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+
+    playerView: {
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: '#FDFF3C',
+        padding: 4, 
+        borderRadius: 20,
+    },
+
+    playerViewCookie: {
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: '#FF1F6C',
+        padding: 4, 
+        borderRadius: 20,
+    },
+
+    player: {
+        color: '#FF1F6C',
+        display: 'flex',
+        flexDirection: 'row',
+        fontSize: 18,
+    },
+
+    playerCookie: {
+        color: '#FDFF3C',
+        display: 'flex',
+        flexDirection: 'row',
+        fontSize: 18,
+    },
+
+    iconPlayerCookie: {
+        display: 'flex',
+        color: '#FDFF3C',
+        fontSize: 22,
+        alignItems: 'center',
+    },
+
+    iconPlayerCupcake: {
+        display: 'flex',
+        color: '#FF1F6C',
+        fontSize: 22,
+        alignItems: 'center',
+    }
 
 });
